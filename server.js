ee5ea78c1d64502beac47f1bd1934bd5bce6109b
JsKim <75792069+Junseok3608@users.noreply.github.com>
2022-08-06 04:33:03 +0900

    FIX: mongodb connect

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 let db;
-const MongoClient = require("mongodb").MongoClient;
-MongoClient.connect("mongodb+srv://jskim:<6l2sn3k4>@cluster0.gmtfm.mongodb.net/?retryWrites=true&w=majority", (error, client) => {
-  // 연결되면 할 일
-  if (error) return console.log(error);
-  db = client.db("todoapp");
+MongoClient.connect("mongodb+srv://jsk:asdfasdf@cluster0.k72gwwy.mongodb.net/?retryWrites=true&w=majority", function (err, client) {
+  // if (err) return console.log(err);
 
-  db.collection("post").insertOne({ 이름: "jsk", 나이: 20 }, function (error, result) {
-    console.log("저장 완료");
+  db = client.db("todoapp");
+  db.collection("post").insertOne({ 이름: "킴스킴", 나이: 15, _id: 100 }, function (err, result) {
+    console.log("complete");
   });
 
   app.listen(8080, function () {
     console.log("listening on 8080");
   });
+  app.post("/add", (req, resp) => {
+    resp.send("Complete");
+    console.log(req.body.title);
+    console.log(req.body.date);
+    db.collection("post").insertOne({ 제목: `${req.body.title}`, 날짜: `${req.body.date}` }, function (err, result) {
+      console.log("complete");
+    });
+  });
 });
 
-// 사용자가 /pet으로 방문하면(get요청하면) 펫 관련된 것 띄워주기
 app.get("/", (req, resp) => {
   resp.sendFile(__dirname + "/index.html");
 });
 app.get("/write", (req, resp) => {
   resp.sendFile(__dirname + "/write.html");
-});
-
-app.post("/add", (req, resp) => {
-  resp.send("Complete");
 });
